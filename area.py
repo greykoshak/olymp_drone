4a4744f7555ee5d58348e3de44a1f80dac1a5235
Igor Agarkov <igor.agarkov.007@gmail.com>
2019-10-31 15:17:59 +0200

    Исправлено: передача SIDE в качестве параметра, ввод ul, br в произвольном порядке

--- area.py
+++ area.py
@@ -1,36 +1,33 @@
-# Шаг для съемки
-SIDE = 50
-
-
 class DefCoord:
     """ Определение массива точек аэрофотосъемки """
 
-    def __init__(self, coord: tuple):
+    def __init__(self, coord: tuple, side):
         self.points = list()
         self.xp1 = coord[0]
         self.yp1 = coord[1]
         self.xp2 = coord[2]
         self.yp2 = coord[3]
+        self.side = side
 
         # main, assign/calculate
         len_x = self.xp2 - self.xp1
         len_y = self.yp2 - self.yp1
 
-        if len_x % SIDE == 0:
-            num_x = len_x // SIDE
+        if len_x % self.side == 0:
+            num_x = len_x // self.side
         else:
-            num_x = len_x // SIDE + 1
+            num_x = len_x // self.side + 1
 
-        if len_y % SIDE == 0:
-            num_y = len_y // SIDE
+        if len_y % self.side == 0:
+            num_y = len_y // self.side
         else:
-            num_y = len_y // SIDE + 1
+            num_y = len_y // self.side + 1
 
         # main, fill
         for i in range(int(num_x)):
             for j in range(int(num_y)):
-                self.points.append((min(self.xp1, self.xp2) + SIDE / 2 + SIDE * i,
-                                    min(self.yp1, self.yp2) + SIDE / 2 + SIDE * j))
+                self.points.append((min(self.xp1, self.xp2) + self.side / 2 + self.side * i,
+                                    min(self.yp1, self.yp2) + self.side / 2 + self.side * j))
 
     def get_area(self):
         return self.points
--- drone_tsp.py
+++ drone_tsp.py
@@ -7,10 +7,11 @@ from tsp import FindRoot
 class DefineArea:
     """ Определение прямоугольника, в котором надо произвести съемку """
 
-    def __init__(self, base, ul, br):
+    def __init__(self, base, ul, br, side):
         self.base = base
         self.top_left = ul
         self.bottom_right = br
+        self.side = side
         self.new_route = None
         self.set_area()
 
@@ -18,7 +19,7 @@ class DefineArea:
 
         # Получить массив координат для съемки
         area = DefCoord((self.top_left[0], self.top_left[1],
-                         self.bottom_right[0], self.bottom_right[1]))
+                         self.bottom_right[0], self.bottom_right[1]), self.side)
         coord = area.get_area()
         print("Points: ", coord)
         coord.insert(0, self.base)
@@ -33,25 +34,30 @@ class DefineArea:
 
 def main(args_list: list):
 
-    base, ul, br = (args_list[0], args_list[1]), (args_list[2], args_list[3]), (args_list[4], args_list[5])
-    print("BASE={}, UL={}, BR={}".format(base, ul, br))
+    base, side = (args_list[0], args_list[1]), args_list[6]
+    points = [(args_list[2], args_list[3]), (args_list[4], args_list[5])]
+    points = sorted(points, key=lambda x: x[0])
+    ul = points[0]
+    br = points[1]
 
-    da = DefineArea(base, ul, br)  # Определить область съемки
+    print("BASE={}, UL={}, BR={}, Side={}".format(base, ul, br, side))
+
+    da = DefineArea(base, ul, br, side)  # Определить область съемки
     return da.get_route()  # tsp route
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 7:
+    if len(sys.argv) == 8:
         try:
             ll = list(map(float, sys.argv[1:]))
         except:
             print('Incorrect numbers', sys.argv[1:])
             sys.exit(1)
     else:
-        print("Number of input parameters must be 6: Base(x,y), UL(x,y), BR(x,y)", sys.argv[1:])
+        print("Number of input parameters must be 6: Base(x1,y1), Rect(x2,y2,x3,y3), Side(n)", sys.argv[1:])
         sys.exit(1)
 
-    if all(item >= 0 for item in ll) and (ll[2] < ll[4]) and (ll[3] < ll[5]):
+    if ll[6] > 0:
         main(ll)
     else:
-        print("Check if >= 0 and UpperLeft < BottomRight", ll)
+        print("Check if Side > 0: ", ll[6])
